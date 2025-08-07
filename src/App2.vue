43f76d61534a68de03dd5e0ff70a7538acf73462
ajaxzheng <894103554@qq.com>
2025-08-07 18:49:36 +0800

    fix: 更新构建脚本并更改主应用组件

---
+++ src/App2.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts" vapor>
+import { ref } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+const message = ref("Vite + Vue");
+const handleClick = () => {
+  message.value = "Hello World!"; // Update the message when the button is clicked
+};
+</script>
+
+<template>
+  <button @click="handleClick">点击我改变值</button>
+  <HelloWorld v-for="item in 30000" :msg="message" />
+</template>
+
+<style scoped>
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: aquamarine;
+  cursor: pointer;
+}
+</style>
